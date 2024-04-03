0e632b8684c47d47f47f4d164649afce9d968699
Mahdi J. Khan <mahdikhan505@gmail.com>
2024-04-03 19:25:50 -0400

    Code clean up :D

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import requests
-import string
 
 v = True
 while v:
-    #Get length of constructed float
+    # Get length of constructed float
     while True:
         length = input("Enter Final Constructed Value Length [2-20]: ")
 
         if not length.isdigit() or (int(length) < 2 or int(length) > 20):
             print("Error: Input not accepted \n")
             continue
-        else: 
+        else:
             break
 
-    #Pull true random numbers from random.org
-    source = "https://www.random.org/decimal-fractions/?num=" + str(length) + "&dec=" + str(length) + "&col=1&base=10&format=plain&rnd=new"
+    # Pull true random numbers from random.org
+    source = (
+        "https://www.random.org/decimal-fractions/?num="
+        + str(length)
+        + "&dec="
+        + str(length)
+        + "&col=1&base=10&format=plain&rnd=new"
+    )
 
     num = (requests.get(source).text).split()
 
-    #Init final string
-    fin = '0.'
+    # Init final string
+    fin = "0."
 
     print("Random Generation Set: ")
     for i in range(int(length)):
-        
         temp = num[i]
-        
-        #add +1 or -1 
-        print(str('%2d' % (i+1)) + ": " + str(temp))
 
-        temp2 = int(temp[i+2])
+        # add +1 or -1
+        print(str("%2d" % (i + 1)) + ": " + str(temp))
+
+        temp2 = int(temp[i + 2])
 
         if temp2 == 9:
             digit = temp2 - 1
@@ -42,15 +46,14 @@ while v:
 
     while True:
         proceed_status = input("Generate More? (y/n): ")
-        
+
         print("")
 
-        if proceed_status in ["y","Y","yes","Yes","YES"]:
+        if proceed_status in ["y", "Y", "yes", "Yes", "YES"]:
             break
-        elif proceed_status in ["n","N","no","No","NO"]:
+        elif proceed_status in ["n", "N", "no", "No", "NO"]:
             v = False
             break
         else:
             print("Error: Input not accepted\n")
             continue
-
